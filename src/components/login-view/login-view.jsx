2fddf148f7d90c19f1c4b3d9b603dccb8f56efb2
melwhite1 <melissawhite668@gmail.com>
2022-08-30 09:31:51 -0400

    Added quote to login view

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react
+import React, { useState } from 'react'
 import PropTypes from "prop-types";
 import axios from 'axios';
 import { Form, Button, Card, CardGroup, Container, Col, Row, Navbar, Nav } from 'react-bootstrap';
